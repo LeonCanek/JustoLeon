d25e8d20cd4ddbcc5591d8128e8e047ad869b7f0
Justo Leon <franciscoleonpastor@gmail.com>
2022-10-19 00:51:37 -0500

    Merge remote-tracking branch 'origin/nueva_rama' into nueva_rama

---
+++ src/t2_herencia/HerenciaPaco.java
@@ -0,0 +1,45 @@
+package t2_herencia;
+class Dios {
+    static{
+        System.out.println("Dios");
+    }
+    void h(){
+        System.out.println("Soy dios");
+    }
+}
+class Hijo extends Dios{
+    static{
+        System.out.println("hijo");
+    }
+    void h(){
+        System.out.println("Soy hijo");
+    }
+}
+class EspirituSanto extends Hijo{
+    static{
+        System.out.println("Espiritu Santo");
+    }
+    void h(){
+        System.out.println("EspituSanto");
+    }
+}
+
+class Justo extends EspirituSanto{
+    static{
+        System.out.println("Justo");
+    }
+    void h() {
+        System.out.println("Justo");
+    }
+}
+
+public class HerenciaPaco extends Justo {
+    public static void main(String[] args){
+        Dios j = new Dios();
+        j.h();
+        System.out.println((Justo)j);
+        // como imprimir soy justo como realizar correctamente el downcasting
+
+    }
+}
+
