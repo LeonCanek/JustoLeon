c9c20233a47ef8ec1f6a000188a98b2d9ea803c4
Justo Leon <franciscoleonpastor@gmail.com>
2022-10-18 21:01:33 -0500

    HoneyComb 3.0

--- src/t2_herencia/Herencia02.java
+++ src/t2_herencia/Herencia02.java
@@ -6,25 +6,20 @@ class SuperExterna {
         }
     }
 }
-class SubExterna extends  SuperExterna{
+class SubExterna extends  SuperExterna{/*
     class Interna{
         void m(){
             System.out.println("Sub");
         }
-        void a(){
-            System.out.println("Augusto");
-        }
-    }
+    }*/
 }
 public class Herencia02 {
     public static void main(String[] args){
         SuperExterna.Interna sei = new SuperExterna().new Interna();
         sei.m();
-
         SubExterna.Interna sei2 = new SubExterna().new Interna();
         sei2.m();
-        sei2.a();/*
         SuperExterna.Interna sei3 = new SubExterna().new Interna();
-        sei3.m();*/
+        sei3.m();
     }
 }
